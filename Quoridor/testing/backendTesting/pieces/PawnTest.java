5560151a75a325089328413e3088ef3b07d82fd1
Collen Roller <rollercw195@potsdam.edu>
2014-05-13 13:33:50 -0400

    ai removing fixed, and testing working

--- Quoridor/testing/backendTesting/pieces/PawnTest.java
+++ Quoridor/testing/backendTesting/pieces/PawnTest.java
@@ -6,15 +6,9 @@ package backendTesting.pieces;
 
 import java.awt.Image;
 import java.awt.Toolkit;
-import java.io.IOException;
-import java.net.UnknownHostException;
-import java.util.*;
-import junit.framework.*;
 
-
+import junit.framework.TestCase;
 import quoridor.backend.containers.Position;
-import quoridor.main.Quoridor;
-import quoridor.network.client.NetworkClient;
 import quoridor.backend.pieces.Pawn;
 
 
